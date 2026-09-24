73971956434cc37a161dbab3c330ab8c1eb4ac38
Brayan Segovia <71364680+Brayan-byte@users.noreply.github.com>
2026-09-23 19:07:27 -0600

    Add: sql-auto-increment - submission-0

---
+++ SQL/sql-auto-increment/submission-0.sql
@@ -0,0 +1,24 @@
+create sequence gov_id START WITH 1000 increment by 3;
+
+create table  gov_employee(
+  id integer generated always as identity,
+  gov_id Integer default nextval('gov_id'),
+  name text
+);
+
+
+
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO gov_employee (name) 
+  VALUES
+      ('John Doe'),
+      ('Jane Doe'),
+      ('Jim Beam');
+
+SELECT * FROM gov_employee;
